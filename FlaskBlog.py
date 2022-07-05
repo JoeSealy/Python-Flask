01c4a6a806c05fbdd91349b376ce188626e2c84c
Joe Sealy <Joe.Sealy@hotmail.co.uk>
2022-07-05 11:18:50 +0100

    new stuff

--- FlaskBlog.py
+++ FlaskBlog.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
+app.config["SECRET_KEY"] = "a324537758596f5ae524d5fcdd8790a4"
 posts = [
     {
         "author":"Joe Sealy",
@@ -27,7 +28,15 @@ def home():
 def about():
     return render_template("about.html", title = "About")
 
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template("Register.html", title  = "Register", form = form)
 
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title  = "Login", form = form)
 
 if __name__ == "__main__":
     app.run(debug=True)
