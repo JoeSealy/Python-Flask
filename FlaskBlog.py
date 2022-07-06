bfd967b4fbb4a92639a489b36830ef25a1bb9696
Joe Sealy <Joe.Sealy@hotmail.co.uk>
2022-07-06 15:36:04 +0100

    got it all fixed now and added the login page too all works with error prompts telling the user if the inputs are wrong

--- FlaskBlog.py
+++ FlaskBlog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, redirect, url_for
+from flask import Flask, render_template, flash, redirect,  url_for
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -34,12 +34,19 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         flash(f"Account created for {form.username.data}!", "success")
-        return (redirect(url_for("home")))
+        return redirect(url_for("home"))
+
     return render_template("register.html", title  = "Register", form = form)
 
-@app.route("/login")
+@app.route("/login",  methods = ["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash("You have been logged in!", "success")
+            return redirect(url_for("home"))
+        else:
+            flash("Login unsuccessfull. Please check username or password", "danger")
     return render_template("login.html", title  = "Login", form = form)
 
 if __name__ == "__main__":
