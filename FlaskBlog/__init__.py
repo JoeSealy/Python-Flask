8d65610f6282e6e84935c0e65f425fcd4e0b3afd
Joe Sealy <Joe.Sealy@hotmail.co.uk>
2022-07-29 02:26:58 +0100

    added FlaskBlog

--- FlaskBlog/__init__.py
+++ FlaskBlog/__init__.py
@@ -3,10 +3,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "a324537758596f5ae524d5fcdd8790a4"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 from FlaskBlog import routes
--- FlaskBlog/forms.py
+++ FlaskBlog/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from FlaskBlog.models import User
 
 
@@ -14,8 +14,14 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField("Sign Up")
 
     def validate_username(self, username):
-        if True:
-            raise validatorError("Validation Message")
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("That Username is taken")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("That Email is taken")
 
 
 class LoginForm(FlaskForm):
