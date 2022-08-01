5c706a867e64e7ce30365c281d1e833428d89925
Joe Sealy <Joe.Sealy@hotmail.co.uk>
2022-08-01 01:35:58 +0100

    starter added to accounts page

--- FlaskBlog/routes.py
+++ FlaskBlog/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, flash, redirect,  url_for
+from flask import render_template, flash, redirect,  url_for, request
 from FlaskBlog.forms import RegistrationForm, LoginForm
 from FlaskBlog.models import User, Post
 from FlaskBlog import app, db, bcrypt
-from Flask_login import login_user
+from flask_login import login_user, current_user, logout_user, login_required
 posts = [
     {
         "author": "Joe Sealy",
@@ -32,6 +32,8 @@ def about():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(
@@ -48,12 +50,25 @@ def register():
 
 @app.route("/login",  methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember = form.remember.data)
-            return redirect(url_for("home"))
+            next_page = request.args.get("next")
+            return redirect (next_page) if next_page else redirect(url_for("home")) #ternary conditional
         else:
             flash("Login unsuccessfull. Please check username or password", "danger")
     return render_template("login.html", title="Login", form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+@app.route("/account")
+@login_required
+def account():
+     return render_template("account.html", title="Account")
