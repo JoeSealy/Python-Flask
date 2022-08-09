1033944750f34e62cee19e03fcad910ef2051c22
Joe Sealy <Joe.Sealy@hotmail.co.uk>
2022-08-09 01:40:58 +0100

    should probably hide my database key for security purposes respectivley

--- FlaskBlog/__init__.py
+++ FlaskBlog/__init__.py
@@ -4,9 +4,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import key
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "a324537758596f5ae524d5fcdd8790a4"
+app.config["SECRET_KEY"] = key.SECRET_KEY
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
