4b3ff758d62cab32ac214129e29953b7efe9f337
Joe Sealy <Joe.Sealy@hotmail.co.uk>
2022-08-10 01:44:08 +0100

    add pagination with access to the rest of the posts

--- FlaskBlog/__init__.py
+++ FlaskBlog/__init__.py
@@ -1,13 +1,13 @@
-
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-import key
+from FlaskBlog import personal
+
+
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = key.SECRET_KEY
+app.config["SECRET_KEY"] = personal.SECRET_KEY
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
--- Run.py
+++ Run.py
@@ -1,4 +1,5 @@
 from FlaskBlog import app
 
 if __name__ == "__main__":
+    app
     app.run(debug=True)
