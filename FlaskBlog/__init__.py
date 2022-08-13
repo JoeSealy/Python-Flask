d4fe10b8793ae58b627333ea96e35e667e1d96b2
Joe Sealy <Joe.Sealy@hotmail.co.uk>
2022-08-13 02:46:18 +0100

    forgot different import but still doest work

--- FlaskBlog/__init__.py
+++ FlaskBlog/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from FlaskBlog import personal
-from flaskext.mail import Mail
+from flask_mail import Mail
 
 
 
